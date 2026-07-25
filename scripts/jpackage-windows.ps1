fb0e71c120e1570585c1e6664f323590109f367d
durotarbg <durotarbg@gmail.com>
2026-07-25 22:19:18 +0300

    Release v1.2.0

--- scripts/jpackage-windows.ps1
+++ scripts/jpackage-windows.ps1
@@ -13,7 +13,7 @@ param(
 
 $ErrorActionPreference = "Stop"
 $root = Split-Path $PSScriptRoot -Parent
-$version = "1.1.9"
+$version = "1.2.0"
 $jarName = "win-zenith-$version-shaded.jar"
 $jar = Join-Path $root "target\$jarName"
 if (-not (Test-Path $jar)) {
